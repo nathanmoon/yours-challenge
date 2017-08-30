8d500dbba143aeed1771592c2f9e8d2b5f0c3ddb
Nathan Moon <nathannospam@gmail.com>
2017-08-30 20:14:22 +0000

    basic right and left turn lane functionality

--- traffic.py
+++ traffic.py
@@ -9,7 +9,9 @@ E = (1,0)
 S = (0,-1)
 W = (-1,0)
 NW = (-1,1)
+NE = (1,1)
 SE = (1,-1)
+SW = (-1,-1)
 
 # grid size
 GRID_MIN = 0
@@ -17,24 +19,26 @@ GRID_MAX = 13
 
 GRID_RANGE = range(GRID_MIN, GRID_MAX)
 
+GRID_MID = GRID_MAX // 2
+
 # coordinates of the various lanes
-NORTHBOUND1 = [(7,y) for y in GRID_RANGE]
-NORTHBOUND2 = [(8,y) for y in GRID_RANGE]
-SOUTHBOUND1 = [(5,y) for y in GRID_RANGE]
-SOUTHBOUND2 = [(4,y) for y in GRID_RANGE]
-WESTBOUND1 = [(x,7) for x in GRID_RANGE]
-WESTBOUND2 = [(x,8) for x in GRID_RANGE]
-EASTBOUND1 = [(x,4) for x in GRID_RANGE]
-EASTBOUND2 = [(x,5) for x in GRID_RANGE]
-NORTHRIGHT = [(9,y) for y in range(GRID_MIN,4)]
-SOUTHRIGHT = [(3,y) for y in range(9,GRID_MAX)]
-EASTRIGHT = [(x,3) for x in range(GRID_MIN,4)]
-WESTRIGHT = [(x,9) for x in range(9,GRID_MAX)]
-NORTHLEFT = [(6,y) for y in range(GRID_MIN,4)]
-SOUTHLEFT = [(6,y) for y in range(9,GRID_MAX)]
-EASTLEFT = [(x,6) for x in range(GRID_MIN,4)]
-WESTLEFT = [(x,6) for x in range(9,GRID_MAX)]
-MIDDLE = [(6,6)]
+NORTHBOUND1 = [(GRID_MID+1,y) for y in GRID_RANGE]
+NORTHBOUND2 = [(GRID_MID+2,y) for y in GRID_RANGE]
+SOUTHBOUND1 = [(GRID_MID-1,y) for y in GRID_RANGE]
+SOUTHBOUND2 = [(GRID_MID-2,y) for y in GRID_RANGE]
+WESTBOUND1 = [(x,GRID_MID+1) for x in GRID_RANGE]
+WESTBOUND2 = [(x,GRID_MID+2) for x in GRID_RANGE]
+EASTBOUND1 = [(x,GRID_MID-2) for x in GRID_RANGE]
+EASTBOUND2 = [(x,GRID_MID-1) for x in GRID_RANGE]
+NORTHRIGHT = [(GRID_MID+3,y) for y in range(GRID_MIN,GRID_MID-2)]
+SOUTHRIGHT = [(GRID_MID-3,y) for y in range(GRID_MID+3,GRID_MAX)]
+EASTRIGHT = [(x,GRID_MID-3) for x in range(GRID_MIN,GRID_MID-2)]
+WESTRIGHT = [(x,GRID_MID+3) for x in range(GRID_MID+3,GRID_MAX)]
+NORTHLEFT = [(GRID_MID,y) for y in range(GRID_MIN,GRID_MID)]
+SOUTHLEFT = [(GRID_MID,y) for y in range(GRID_MID+1,GRID_MAX)]
+EASTLEFT = [(x,GRID_MID) for x in range(GRID_MIN,GRID_MID)]
+WESTLEFT = [(x,GRID_MID) for x in range(GRID_MID+1,GRID_MAX)]
+MIDDLE = [(GRID_MID,GRID_MID)]
 
 LANES = NORTHBOUND1 + NORTHBOUND2 + SOUTHBOUND1 + SOUTHBOUND2 + \
         EASTBOUND1 + EASTBOUND2 + WESTBOUND1 + WESTBOUND2 + \
@@ -45,50 +49,67 @@ LANES = NORTHBOUND1 + NORTHBOUND2 + SOUTHBOUND1 + SOUTHBOUND2 + \
 # coordinates of the lights.
 # basically the entry points into the intersection, for each lane
 # a car may not move into a coordinate that is a red light
-NORTH_LEFT_LIGHT = (6,3)
-NORTH1_LIGHT = (7,3)
-NORTH2_LIGHT = (8,3)
-NORTHRIGHT_LIGHT = (9,3)
-SOUTH_LEFT_LIGHT = (6,9)
-SOUTH1_LIGHT = (5,9)
-SOUTH2_LIGHT = (4,9)
-SOUTHRIGHT_LIGHT = (3,9)
-EAST_LEFT_LIGHT = (3,6)
-EAST1_LIGHT = (3,5)
-EAST2_LIGHT = (3,4)
-EASTRIGHT_LIGHT = (3,3)
-WEST_LEFT_LIGHT = (9,6)
-WEST1_LIGHT = (9,7)
-WEST2_LIGHT = (9,8)
-WESTRIGHT_LIGHT = (9,9)
+NORTH_LEFT_LIGHT = (GRID_MID,GRID_MID-3)
+NORTH1_LIGHT = (GRID_MID+1,GRID_MID-3)
+NORTH2_LIGHT = (GRID_MID+2,GRID_MID-3)
+NORTHRIGHT_LIGHT = (GRID_MID+3,GRID_MID-3)
+SOUTH_LEFT_LIGHT = (GRID_MID,GRID_MID+3)
+SOUTH1_LIGHT = (GRID_MID-1,GRID_MID+3)
+SOUTH2_LIGHT = (GRID_MID-2,GRID_MID+3)
+SOUTHRIGHT_LIGHT = (GRID_MID-3,GRID_MID+3)
+EAST_LEFT_LIGHT = (GRID_MID-3,GRID_MID)
+EAST1_LIGHT = (GRID_MID-3,GRID_MID-1)
+EAST2_LIGHT = (GRID_MID-3,GRID_MID-2)
+EASTRIGHT_LIGHT = (GRID_MID-3,GRID_MID-3)
+WEST_LEFT_LIGHT = (GRID_MID+3,GRID_MID)
+WEST1_LIGHT = (GRID_MID+3,GRID_MID+1)
+WEST2_LIGHT = (GRID_MID+3,GRID_MID+2)
+WESTRIGHT_LIGHT = (GRID_MID+3,GRID_MID+3)
 
-NORTH_LIGHTS = [NORTH_LEFT_LIGHT, NORTH1_LIGHT, NORTH2_LIGHT, NORTHRIGHT_LIGHT]
-SOUTH_LIGHTS = [SOUTH_LEFT_LIGHT, SOUTH1_LIGHT, SOUTH2_LIGHT, SOUTHRIGHT_LIGHT]
-EAST_LIGHTS = [EAST_LEFT_LIGHT, EAST1_LIGHT, EAST2_LIGHT, EASTRIGHT_LIGHT]
-WEST_LIGHTS = [WEST_LEFT_LIGHT, WEST1_LIGHT, WEST2_LIGHT, WESTRIGHT_LIGHT]
+NORTH_LIGHTS = [NORTH1_LIGHT, NORTH2_LIGHT, NORTHRIGHT_LIGHT]
+NORTH_LEFT_LIGHTS = [NORTH_LEFT_LIGHT]
+SOUTH_LIGHTS = [SOUTH1_LIGHT, SOUTH2_LIGHT, SOUTHRIGHT_LIGHT]
+SOUTH_LEFT_LIGHTS = [SOUTH_LEFT_LIGHT]
+EAST_LIGHTS = [EAST1_LIGHT, EAST2_LIGHT, EASTRIGHT_LIGHT]
+EAST_LEFT_LIGHTS = [EAST_LEFT_LIGHT]
+WEST_LIGHTS = [WEST1_LIGHT, WEST2_LIGHT, WESTRIGHT_LIGHT]
+WEST_LEFT_LIGHTS = [WEST_LEFT_LIGHT]
 
-LIGHTS = NORTH_LIGHTS + SOUTH_LIGHTS + EAST_LIGHTS + WEST_LIGHTS
+LIGHTS = NORTH_LIGHTS + NORTH_LEFT_LIGHTS + \
+        SOUTH_LIGHTS + SOUTH_LEFT_LIGHTS + \
+        EAST_LIGHTS + EAST_LEFT_LIGHTS + \
+        WEST_LIGHTS + WEST_LEFT_LIGHTS
 
 NS_LIGHTS = NORTH_LIGHTS + SOUTH_LIGHTS
+NS_LEFT_LIGHTS = NORTH_LEFT_LIGHTS + SOUTH_LEFT_LIGHTS
 EW_LIGHTS = EAST_LIGHTS + WEST_LIGHTS
+EW_LEFT_LIGHTS = EAST_LEFT_LIGHTS + WEST_LEFT_LIGHTS
 
 # these are in order:
 LIGHT_STATES = [
         'NS_GREEN',
         'NS_YELLOW',
         'NS_ALL_RED', # waiting for intersection to clear
+        'NS_LEFT_GREEN',
+        'NS_LEFT_YELLOW',
         'EW_GREEN',
         'EW_YELLOW',
         'EW_ALL_RED', # waiting for intersection to clear
+        'EW_LEFT_GREEN',
+        'EW_LEFT_YELLOW',
         ]
 # how long to remain in each state
 LIGHT_STATE_DURATIONS = {
-        'NS_GREEN': 8,
+        'NS_GREEN': GRID_MID+2,
         'NS_YELLOW': 1,
-        'NS_ALL_RED': 5,
-        'EW_GREEN': 8,
+        'NS_ALL_RED': 3,
+        'NS_LEFT_GREEN': 4,
+        'NS_LEFT_YELLOW': 1,
+        'EW_GREEN': GRID_MID+2,
         'EW_YELLOW': 1,
-        'EW_ALL_RED': 5,
+        'EW_ALL_RED': 3,
+        'EW_LEFT_GREEN': 4,
+        'EW_LEFT_YELLOW': 1,
         }
 light_state = 'NS_GREEN'
 light_state_counter = 0
@@ -97,28 +118,12 @@ RED = 'red'
 YELLOW = 'yellow'
 GREEN = 'green'
 
-def get_light_value(coord):
-    global light_state
-    if light_state == 'NS_GREEN':
-        if coord in NORTH_LIGHTS or coord in SOUTH_LIGHTS:
-            return GREEN
-    if light_state == 'NS_YELLOW':
-        if coord in NORTH_LIGHTS or coord in SOUTH_LIGHTS:
-            return YELLOW
-    if light_state == 'EW_GREEN':
-        if coord in EAST_LIGHTS or coord in WEST_LIGHTS:
-            return GREEN
-    if light_state == 'EW_YELLOW':
-        if coord in EAST_LIGHTS or coord in WEST_LIGHTS:
-            return YELLOW
-    return RED if is_light(coord) else None
-
 
 # coordinate, direction pairs for where cars may enter the grid
-NORTH_INITIALIZERS = [((7,-1), N),((8,-1), N)]
-SOUTH_INITIALIZERS = [((4,13), S),((5,13), S)]
-EAST_INITIALIZERS = [((-1,4), E), ((-1,5), E)]
-WEST_INITIALIZERS = [((13,7), W), ((13,8), W)]
+NORTH_INITIALIZERS = [((GRID_MID+1,GRID_MIN-1), N),((GRID_MID+2,GRID_MIN-1), N), ((GRID_MID+3,GRID_MIN-1), N), ((GRID_MID,GRID_MIN-1), N)]
+SOUTH_INITIALIZERS = [((GRID_MID-2,GRID_MAX), S),((GRID_MID-1,GRID_MAX), S), ((GRID_MID-3,GRID_MAX), S), ((GRID_MID,GRID_MAX), S)]
+EAST_INITIALIZERS = [((GRID_MIN-1,GRID_MID-2), E), ((GRID_MIN-1,GRID_MID-1), E), ((GRID_MIN-1,GRID_MID-3), E), ((GRID_MIN-1,GRID_MID), E)]
+WEST_INITIALIZERS = [((GRID_MAX,GRID_MID+1), W), ((GRID_MAX,GRID_MID+2), W), ((GRID_MAX,GRID_MID+3), W), ((GRID_MAX,GRID_MID), W)]
 
 INITIALIZERS = NORTH_INITIALIZERS + SOUTH_INITIALIZERS + EAST_INITIALIZERS + WEST_INITIALIZERS
 
@@ -129,6 +134,7 @@ INITIALIZERS = NORTH_INITIALIZERS + SOUTH_INITIALIZERS + EAST_INITIALIZERS + WES
 # typically continue in their lane
 TRANSITIONS = defaultdict(dict)
 
+# straight lane transitions
 for coord in NORTHBOUND1 + NORTHBOUND2:
     TRANSITIONS[coord][N] = N # northbound continues northbound
 for coord in SOUTHBOUND1 + SOUTHBOUND2:
@@ -137,6 +143,41 @@ for coord in EASTBOUND1 + EASTBOUND2:
     TRANSITIONS[coord][E] = E # eastbount continues eastbount
 for coord in WESTBOUND1 + WESTBOUND2:
     TRANSITIONS[coord][W] = W # westbount continues westbount
+# right turn lane transitions
+for coord in NORTHRIGHT:
+    TRANSITIONS[coord][N] = N
+TRANSITIONS[(GRID_MID+3,GRID_MID-2)][N] = E
+for coord in SOUTHRIGHT:
+    TRANSITIONS[coord][S] = S
+TRANSITIONS[(GRID_MID-3,GRID_MID+2)][S] = W
+for coord in EASTRIGHT:
+    TRANSITIONS[coord][E] = E
+TRANSITIONS[(GRID_MID-2,GRID_MID-3)][E] = S
+for coord in WESTRIGHT:
+    TRANSITIONS[coord][W] = W
+TRANSITIONS[(GRID_MID+2,GRID_MID+3)][W] = N
+# left turn lane transitions
+for coord in NORTHLEFT:
+    TRANSITIONS[coord][N] = N
+TRANSITIONS[(GRID_MID,GRID_MID-1)][N] = NW
+TRANSITIONS[(GRID_MID-1,GRID_MID)][NW] = NW
+TRANSITIONS[(GRID_MID-2,GRID_MID+1)][NW] = W
+for coord in SOUTHLEFT:
+    TRANSITIONS[coord][S] = S
+TRANSITIONS[(GRID_MID,GRID_MID+1)][S] = SE
+TRANSITIONS[(GRID_MID+1,GRID_MID)][SE] = SE
+TRANSITIONS[(GRID_MID+2,GRID_MID-1)][SE] = E
+for coord in EASTLEFT:
+    TRANSITIONS[coord][E] = E
+TRANSITIONS[(GRID_MID-1,GRID_MID)][E] = NE
+TRANSITIONS[(GRID_MID,GRID_MID+1)][NE] = NE
+TRANSITIONS[(GRID_MID+1,GRID_MID+2)][NE] = N
+for coord in WESTLEFT:
+    TRANSITIONS[coord][W] = W
+TRANSITIONS[(GRID_MID+1,GRID_MID)][W] = SW
+TRANSITIONS[(GRID_MID,GRID_MID-1)][SW] = SW
+TRANSITIONS[(GRID_MID-1,GRID_MID-2)][SW] = S
+
 
 # tracks all the cars on the grid
 all_cars = []
@@ -168,12 +209,11 @@ class Car:
             # clean this reference to all_cars_set up
             all_cars_set.remove(self.coord)
             self.coord = self.next_coord()
-            self.direction = TRANSITIONS[self.coord][self.direction]
+            self.direction = TRANSITIONS.get(self.coord, {}).get(self.direction, (0,0))
             all_cars_set.add(self.coord)
 
     def is_done(self):
-        next_coord = self.next_coord()
-        return is_outside_grid(next_coord)
+        return is_outside_grid(self.coord)
 
 # helpers
 def is_occupied(coord):
@@ -187,6 +227,34 @@ def is_lane(coord):
 
 def is_outside_grid(coord):
     return coord[0] < GRID_MIN or coord[0] >= GRID_MAX or coord[1] < GRID_MIN or coord[1] >= GRID_MAX
+
+def get_light_value(coord):
+    global light_state
+    if light_state == 'NS_GREEN':
+        if coord in NORTH_LIGHTS or coord in SOUTH_LIGHTS:
+            return GREEN
+    if light_state == 'NS_YELLOW':
+        if coord in NORTH_LIGHTS or coord in SOUTH_LIGHTS:
+            return YELLOW
+    if light_state == 'NS_LEFT_GREEN':
+        if coord in NORTH_LEFT_LIGHTS or coord in SOUTH_LEFT_LIGHTS:
+            return GREEN
+    if light_state == 'NS_LEFT_YELLOW':
+        if coord in NORTH_LEFT_LIGHTS or coord in SOUTH_LEFT_LIGHTS:
+            return YELLOW
+    if light_state == 'EW_GREEN':
+        if coord in EAST_LIGHTS or coord in WEST_LIGHTS:
+            return GREEN
+    if light_state == 'EW_YELLOW':
+        if coord in EAST_LIGHTS or coord in WEST_LIGHTS:
+            return YELLOW
+    if light_state == 'EW_LEFT_GREEN':
+        if coord in EAST_LEFT_LIGHTS or coord in WEST_LEFT_LIGHTS:
+            return GREEN
+    if light_state == 'EW_LEFT_YELLOW':
+        if coord in EAST_LEFT_LIGHTS or coord in WEST_LEFT_LIGHTS:
+            return YELLOW
+    return RED if is_light(coord) else None
 
 
 # grid drawing code:
@@ -228,6 +296,14 @@ def advance_lights():
         light_state_counter = 0
         light_state = LIGHT_STATES[(LIGHT_STATES.index(light_state)+1)%len(LIGHT_STATES)]
 
+def remove_finished_cars():
+    global all_cars
+    global all_cars_set
+    finished_cars = [car for car in all_cars if car.is_done()]
+    all_cars[:] = [car for car in all_cars if not car.is_done()]
+    for car in finished_cars:
+        all_cars_set.remove(car.coord)
+
 def advance():
     global all_cars
     global all_cars_set
@@ -235,10 +311,7 @@ def advance():
     add_new_cars()
     for car in all_cars:
         car.move()
-    finished_cars = [car for car in all_cars if car.is_done()]
-    all_cars[:] = [car for car in all_cars if not car.is_done()]
-    for car in finished_cars:
-        all_cars_set.remove(car.coord)
+    remove_finished_cars()
     validate_cars()
 
 def validate_cars():
