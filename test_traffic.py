27e7c112b50de1c35be50649855a76ea91ef22a0
Nathan Moon <nathannospam@gmail.com>
2017-08-30 20:40:50 +0000

    fix tests

--- test_traffic.py
+++ test_traffic.py
@@ -1,43 +1,44 @@
-from traffic import (all_cars, all_cars_set, Car, advance, is_occupied, is_light,
-        is_red_light, red_lights, is_lane, is_outside_grid)
+import pytest
+
+from traffic import (all_cars, all_cars_by_coord, Car, advance, is_occupied, is_light,
+        is_lane, is_outside_grid, GRID_MAX, GRID_MID, N)
+
+def test_car_must_be_in_lane():
+    with pytest.raises(Exception):
+        Car((0,0), N)
+    Car((GRID_MID,0), N)
 
 def test_is_occupied():
-    all_cars_set.clear()
-    all_cars_set.add((0,0))
-    assert is_occupied((0,0))
-    assert not is_occupied((1,1))
+    all_cars_by_coord.clear()
+    all_cars_by_coord[(GRID_MID,0)] = Car((GRID_MID,0), N)
+    assert is_occupied((GRID_MID,0))
+    assert not is_occupied((GRID_MID,1))
 
 def test_is_light():
-    assert is_light((4,9))
-    assert not is_light((6,6))
+    assert is_light((GRID_MID,GRID_MID-3))
+    assert not is_light((GRID_MID,GRID_MID))
 
 def test_is_lane():
-    assert is_lane((6,6))
+    assert is_lane((GRID_MID,GRID_MID))
     assert not is_lane((0,0))
 
 def test_is_outside_grid():
     assert is_outside_grid((-1,0))
     assert not is_outside_grid((0,0))
-    assert is_outside_grid((1,13))
-    assert not is_outside_grid((12,12))
-
-def test_is_red_light():
-    red_lights[:] = []
-    red_lights.append((0,0))
-    assert is_red_light((0,0))
-    assert not is_red_light((1,1))
+    assert is_outside_grid((1,GRID_MAX))
+    assert not is_outside_grid((GRID_MAX-1,GRID_MAX-1))
 
 def test_is_done():
-    car = Car((8,12), (0,1))
+    car = Car((GRID_MID+1,GRID_MAX), (0,1))
     assert car.is_done()
 
 def test_advance_removes_finished_cars():
     all_cars[:] = []
-    all_cars_set.clear()
-    car = Car((8,11), (0,1))
+    all_cars_by_coord.clear()
+    car = Car((GRID_MID+1,GRID_MAX-1), (0,1))
     all_cars.append(car)
-    all_cars_set.add(car.coord)
+    all_cars_by_coord[car.coord] = car
     advance()
     assert len(all_cars) == 1
-    assert len(all_cars_set) == 1
+    assert len(all_cars_by_coord.keys()) == 1
 
